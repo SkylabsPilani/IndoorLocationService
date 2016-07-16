b119f6c3742889a6074d8615fa3a5749fb253c43
Viraj Prabhu <prabhuviraj@gmail.com>
2016-07-17 01:42:04 +0530

    Working routing logic, no DB integration

--- locationservice/locationservice.py
+++ locationservice/locationservice.py
@@ -1,15 +1,12 @@
 import os
 import http.client, urllib.parse
 from flask import Flask, request, session, g, redirect, url_for, abort, \
-    render_template, flash
+    render_template, flash, make_response, jsonify
 import locationservice.dbconns as dbconns
-
+import requests
 # create our little application :)
 app = Flask(__name__)
 
-app.run(host='0.0.0.0')
-
-""" WTF IS THIS BLOCK """
 # Load default config and override config from an environment variable
 app.config.update(dict(
     # DATABASE=os.path.join(app.root_path, 'flaskr.db'),
@@ -22,9 +19,7 @@ app.config.update(dict(
 # Is this needed?
 app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
-""" end of WTF IS THIS BLOCK """
-
-find_server_address = "ml.internalpositioning.com/"
+find_server_address = "ml.internalpositioning.com"
 # find_server_port = "80"
 find_server_protocol = "https"
 find_server_learn_route = "/learn"
@@ -36,12 +31,13 @@ def index():
 
 @app.route('/venueReg', methods=['POST'])
 def venue_registration():
-    payload = request.form['data']
+
+    payload = request.form
+    print(payload)
 
     venue = payload['venue']
-    gps_lat = payload['gps_lat']
+    print(venue)
     gps_long = payload['gps_long']    
-
     #Insert into VENUE table
     if dbconns.put_venue_for_gps(gps_lat, gps_long, venue):
         # This is successful
@@ -53,30 +49,31 @@ def venue_registration():
 
 @app.route('/learn', methods=['POST'])
 def learn():
-    if find_server_protocol == "http" :
-        server_connection = http.client.HTTPConnection(find_server_address)
-    elif find_server_protocol == "https" :
-        server_connection = http.client.HTTPSConnection(find_server_address)
 
     # Parse payload
-    payload = request.form['data']
+    payload = request.get_json()
+    print(payload)
+
     find_learn_payload = payload['find_payload']
+
     group = find_learn_payload['group']
     location = find_learn_payload['location']
+
     coupon_code = payload['coupon_code']
 
     # Make DB Entry (without duplicating)
 
     # Construct call for learn to public server
-    server_connection.request("POST", find_server_learn_route, find_payload)
-    server_response = server_connection.getresponse()
-    print(server_response.status, server_response.read())
-    server_connection.close()
-    return 'Learning'
 
-@app.route('/getVenueForGPS')
+    r = requests.post("https://" + find_server_address + find_server_learn_route, json=find_learn_payload)
+    print(r.json())
+    resp = make_response(r.json())
+    resp.headers['Content-Type'] = "application/json"
+    return resp
+
+@app.route('/getVenueForGPS', methods=['POST'])
 def get_venue_for_gps():
-    payload = request.form['data']
+    payload = request.get_json()
 
     gps_lat = payload['gps_lat']
     gps_long = payload['gps_long']
@@ -86,10 +83,9 @@ def get_venue_for_gps():
     # TODO make the response
 
     # Return venue
-
     return 'Get Venue For GPS'
 
-@app.route('/track')
+@app.route('/track', methods=['POST'])
 def track():
     if find_server_protocol == "http" :
         server_connection = http.client.HTTPConnection(find_server_address)
@@ -97,19 +93,17 @@ def track():
         server_connection = http.client.HTTPSConnection(find_server_address)
 
     # Parse payload
-    payload = request.form['data']
+    payload = request.get_json()
     find_track_payload = payload['find_payload']
     group = find_track_payload['group']
 
     # Construct call for track to public server
-    server_connection.request("POST", find_server_track_route, find_track_payload)
-    server_response = server_connection.getresponse()
-    print(server_response.status, server_response.read())
-    server_connection.close()
+    r = requests.post("https://" + find_server_address + find_server_track_route, json=find_track_payload)
+    print(r.json())
 
     # Get location from response
 
     # Query DB for coupon code and return
-    return 'Tracking'
-
-
+    resp = make_response(r.json())
+    resp.headers['Content-Type'] = "application/json"
+    return resp
