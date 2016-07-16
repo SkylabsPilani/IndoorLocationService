087610c2ec99478bc683780915e10f07e4a2601b
Pushkar <pushkar1593@gmail.com>
2016-07-17 00:50:25 +0530

    Lat and Lon being normalized to string in dbconn

--- locationservice/dbconns.py
+++ locationservice/dbconns.py
@@ -6,7 +6,12 @@ gps_venue_table = dynamodb.Table('GPSVenues')
 gps_venue_table_key = 'GPSCoordinates'
 gps_venue_table_venue_key = 'Venues'
 
-def get_gps_venues(gps_coordinates):
+
+def __normalize_coordinates__(gps_lat, gps_lon):
+    return str(int(float(gps_lat))) + str(int(float(gps_lon)))
+
+
+def __get_gps_venues__(gps_coordinates):
     gps_dict = {gps_venue_table_key:gps_coordinates}
     response = gps_venue_table.get_item(Key=gps_dict)
     try:
@@ -16,7 +21,8 @@ def get_gps_venues(gps_coordinates):
 
     return venues
 
-def put_venue_for_gps(gps_coordinates, venue):
+
+def __put_venue_for_gps__(gps_coordinates, venue):
     venues = get_gps_venues(gps_coordinates)
     if venues is None:
         venues = [venue]
@@ -25,9 +31,17 @@ def put_venue_for_gps(gps_coordinates, venue):
             venues.append(venue)
         else:
             return True
-
     gps_dict = {gps_venue_table_key: gps_coordinates, gps_venue_table_venue_key: venues}
     if gps_venue_table.put_item(Item=gps_dict)['ResponseMetadata']['HTTPStatusCode'] == 200:
         return True
-    else :
+    else:
         return False
+
+
+def get_gps_venues(gps_lat, gps_lon):
+    return __get_gps_venues__(__normalize_coordinates__(gps_lat, gps_lon))
+
+
+def put_venue_for_gps(gps_lat, gps_lon, venue):
+    return __put_venue_for_gps__(__normalize_coordinates__(gps_lat, gps_lon), venue)
+
