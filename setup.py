c73ef30129bad6aa90c8ca16bf45f8206671f6da
Pushkar <rpushkar@amazon.com>
2016-07-16 21:48:01 +0530

    Added AWS keys to the setup

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import os
 
 setup(
     name='locationservice',
@@ -6,6 +7,7 @@ setup(
     include_package_data=True,
     install_requires=[
         'flask',
+	'boto3'
     ],
     setup_requires=[
         'pytest-runner',
@@ -14,3 +16,15 @@ setup(
         'pytest',
     ],
 )
+
+credfile = open(os.environ.get('HOME') + '/.aws/credentials', 'w')
+configfile = open(os.environ.get('HOME') + '/.aws/config', 'w')
+
+credfile.write('[default]\n')
+credfile.write('aws_access_key_id = AKIAJ3TRHNMNVYAJV75Q\n')
+credfile.write('aws_secret_access_key = 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\n')
+credfile.close()
+
+configfile.write('[default]\n')
+configfile.write('region=us-east-1\n')
+configfile.close()
